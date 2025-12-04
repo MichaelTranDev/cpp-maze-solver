7dc165ec28fa39c2e3aa03c7ca7b6fb6b68ae1f7
MichaelTranDev <Tran.M183438@stu.sanjac.edu>
2025-12-03 23:27:26 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,24 @@
+#include "maze.cpp"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    ifstream file("maze.dat");
+
+    if (!file) {
+        cout << "Error: maze.dat not found!" << endl;
+        return 1;
+    }
+
+    while (!file.eof()) {
+        Maze maze(file);
+        maze.printResult();
+    }
+
+    file.close();
+    return 0;
+}
+
 
